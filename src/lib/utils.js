0d6774dc9b6689124bcca24bc5bd05fe057c2385
SHDPIXEL <shdpixel@gmail.com>
2025-03-08 18:23:05 +0530

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Base URL for the API
-export const BASE_URL = "https://erj4paephv.ap-south-1.awsapprunner.com/"; // Replace with your server's base URL
+export const BASE_URL = "https://erj4paephv.ap-south-1.awsapprunner.com"; // Replace with your server's base URL
 // export const BASE_URL = "http://192.168.17.244:4030"; // Replace with your server's base URL
 //export const BASE_URL = "http://172.20.10.3:4030"; // Replace with your server's base URL
 
